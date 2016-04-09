c715a4e604b094d903a6bed619ee30ee36e9208a
Krasimir Tsonev <krasimir@outset.ws>
2016-04-09 07:23:10 +0300

    Source files

---
+++ src/widget.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CSSX from 'react-cssx';
+
+class Widget extends React.Component {
+  render() {
+    return (
+      <CSSX styles={ this.css() }>
+        <form>
+          <p>How are you today?</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, aut sunt. Fugit pariatur quisquam dolor nesciunt saepe et provident vitae consequatur blanditiis expedita!</p>
+          <input type="text" />
+          <button>Answer</button>
+        </form>
+      </CSSX>
+    );
+  }
+  css() {
+    return (
+      <style>
+        form {
+          background: #FFF;
+          border: 3px solid #BADA55;
+          padding: 1em;
+        }
+        p {
+          color: #F00;
+          padding: 0;
+          margin-bottom: 3.5em;
+          font-size: 14px;
+        }
+      </style>
+    );
+  }
+}
+
+const container = document.querySelector('#trialreach-widget');
+
+ReactDOM.render(<Widget />, container);
+
+console.log(`You are using ${React.version} version of React and not ${window.React.version} which is the global one.`);
+
